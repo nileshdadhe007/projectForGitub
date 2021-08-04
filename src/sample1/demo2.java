749f94140d3736cecb36872176a54d4f4d1edaf7
Nilesh <Nilesh@LAPTOP-22R4V3MJ>
2021-08-04 14:56:59 +0530

    [nilesh]:created new class demo2

--- src/sample1/demo2.java
+++ src/sample1/demo2.java
@@ -1,5 +1,12 @@
 package sample1;
 
 public class demo2 {
+	public static void main(String[] args) {
+		System.out.println("GM");
+		System.out.println("GM");
+		System.out.println("GM");
+		System.out.println("GM");
+		System.out.println("GM");
+	}
 
 }
