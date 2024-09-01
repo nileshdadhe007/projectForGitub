e7e15078e70fe178023245d89d473ad9c029bec3
Nilesh Dadhe <nileshdadhe007@gmail.com>
2024-09-01 19:58:17 +0530

    updated code for demo files

---
+++ src/sample1/demo3.java
@@ -0,0 +1,13 @@
+package sample1;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		System.out.println("hi....");
+		System.out.println("my name is nilesh");
+		String name="nilesh dadhe";
+		
+
+	}
+
+}
